113958baf9477c7e8df846dae5be3e84ac7f0988
Ken Bailey <12420619+kengbailey@users.noreply.github.com>
2024-01-04 22:19:01 -0600

    added new functions

--- amit.py
+++ amit.py
@@ -1,4 +1,7 @@
 import yt_dlp
+import whisper
+import sqlite3
+from datetime import datetime
 
 
 def youtube_to_m4a(url_list):
@@ -18,20 +21,81 @@ def youtube_to_m4a(url_list):
             'preferredcodec': 'm4a',
         }]
     }
-
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         for url in url_list:
             error_code = ydl.download([url])
 
+def create_amit_db():
+    """
+    Function to create the amit database and the llm_analysis table.
+    """
+    conn = sqlite3.connect('./data/amit.db')
+    c = conn.cursor()
 
+    # Create ll_analysis table
+    c.execute('''CREATE TABLE IF NOT EXISTS llm_analysis (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                prompt_text TEXT NOT NULL,
+                llm_params TEXT NOT NULL,
+                output_text TEXT NOT NULL,
+                output_stats TEXT,
+                context TEXT,
+                date_created DATETIME DEFAULT CURRENT_TIMESTAMP)''')
+
+    # Create episode transcripts table
+    c.execute('''CREATE TABLE IF NOT EXISTS video_transcription (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                filename TEXT NOT NULL,
+                transcription TEXT NOT NULL,
+                url TEXT NOT NULL,
+                context TEXT,
+                date_created DATETIME DEFAULT CURRENT_TIMESTAMP
+                );''')
+
+    conn.commit()
+    conn.close()
+
+def m4a_to_text(m4a_file):
+    """
+    Transcribes the audio from an M4A file to text.
+
+    Args:
+        m4a_file (str): The path to the M4A file.
+
+    Returns:
+        str: The text transcription of the M4A file.
+    """
+    model = whisper.load_model("base.en")
+    result = model.transcribe(m4a_file)
+    return result["text"]
+
+def insert_video_transcription(filename, transcription, context):
+    """
+    Inserts a video transcription into the database.
+
+    Args:
+        filename (str): The name of the video file.
+        transcription (str): The transcription of the video.
+        url (str): The youtube url of the video.
+
+    Returns:
+        None
+    """
+    conn = sqlite3.connect('./data/amit.db')
+    c = conn.cursor()
+    sql = """INSERT INTO video_transcription (filename, transcription, url) 
+    VALUES (?, ?, ?)"""
+    c.execute(sql, (filename, transcription, context))
+    conn.commit()
+    conn.close()
 
 if __name__ == '__main__':
-    youtube_to_m4a()
 
-"""
-What are we doing today? 
-sqlite storage of analysis
-versioned analysis
-put functions into this file
-entire process we've laid out so far
-"""
+    # youtube_to_m4a(["https://www.youtube.com/watch?v=5AU2beqDoDY"])
+
+    # create_amit_db()
+
+    m4a_file = "data/wheeling_options_001_010324.m4a"
+    result = m4a_to_text(m4a_file)
+    insert_video_transcription(m4a_file, result, "https://www.youtube.com/watch?v=5AU2beqDoDY")
+    
